afc26c93092c5e8b13d904b4edaf042b7a7ba538
viktor <dangerouzua@gmail.com>
2021-07-12 00:12:57 +0300

    added procfile

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://aewxjumzmghuqz:7c3fa637c4eb52b837213b1040c3fb39d14d7522e80697a18aa68e4bfcb18df2@ec2-54-195-76-73.eu-west-1.compute.amazonaws.com:5432/de5nclahn5ni2h'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://discovermobily:5r9VsXSH@localhost:5432/discover-mobily'
 db = SQLAlchemy(app)
-db.create_all()
 migrate = Migrate(app, db)
 
 
