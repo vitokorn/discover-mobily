c0ed1d79cbb5e196a2fac90a996211810030134b
viktor <dangerouzua@gmail.com>
2021-07-22 17:07:41 +0300

    added logs

--- app.py
+++ app.py
@@ -147,7 +147,7 @@ def kod():
     code = request.args.get('code')
     url = 'https://accounts.spotify.com/api/token'
     data = {'grant_type': 'authorization_code', 'code': code, 'redirect_uri': redirect_uri}
-    req = requests.post(url=url, data=json.dumps(data))
+    req = requests.post(url=url, data=data)
     test = req.json()
     if req.status_code == 401:
         return render_template('401.html')
